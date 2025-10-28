e0a493333a7589af29985c5624fa831726f33a20
Adelana107 <adelana787898@gmail.com>
2025-10-28 15:13:36 +0100

    Adding Fake Authentication Protecting a Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,45 @@ import Pricingpage from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import ProtectedRoute from "./pages/ProtectedRoutes";
 
 function App() {
   return (
-    <CitiesProvider>
-      {" "}
-      <BrowserRouter>
-        <Routes>
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricingpage />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route index element={<Homepage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        {" "}
+        <BrowserRouter>
+          <Routes>
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricingpage />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />{" "}
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route index element={<Homepage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
